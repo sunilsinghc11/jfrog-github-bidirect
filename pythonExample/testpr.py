55b28f40ee412790e1578a82d29734b26028dbb5
sunilsinghc11 <166667153+sunilsinghc11@users.noreply.github.com>
2024-08-14 11:26:04 -0400

    Update testpr.py

--- pythonExample/testpr.py
+++ pythonExample/testpr.py
@@ -13,7 +13,7 @@ import hashlib
 def arc4_encrypt_password(key, password):
     cipher = ARC4.new(key.encode('utf-8'))
     encrypted_password = cipher.encrypt(password.encode('utf-8'))
-    return hashlib.md5(encrypted_password).hexdigest()
+    return hashlib.sha256(encrypted_password).hexdigest()
 
 # Now you can call printme function
 printme("Hello from JFROG");
